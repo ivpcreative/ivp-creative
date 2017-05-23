da0f3b21cb0e49fc33f0e73b2da629271e393440
ivpcreative <ivpcreative@gmail.com>
2017-05-23 16:31:00 +0900

    wakai:CSS js change

--- themes/weluka-theme-00-child/js/front.js
+++ themes/weluka-theme-00-child/js/front.js
@@ -1,12 +1,12 @@
 /* front.js；管理画面やWelukaページビルダーで動作させたくない。初期状態がhiddenなど。
-/*load時にKICK*/
+load時にKICK*/
 jQuery(function () {
     frontJsObj = new frontJs();     
 });
 /*end.load時にkick*/
 
 /* subJsオブジェクト生成コンストラクタ */
-var frontJsObj = function () {
+var frontJs = function () {
         
 
     } //end.subJs
