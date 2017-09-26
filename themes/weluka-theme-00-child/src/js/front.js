a69dd184d334bda9d62d3fee18f510e402fe24d2
IVP-WAKAI-PC\wakai <ivpcreative@gmail.com>
2017-09-26 11:29:01 +0900

    wakai:不要なCSS,JSのカット、Front.js、アメーションJSの追加

---
+++ themes/weluka-theme-00-child/src/js/front.js
@@ -0,0 +1,41 @@
+/* front.js；管理画面やWelukaページビルダーで動作させたくない。初期状態がhiddenなど。
+load時にKICK*/
+jQuery(function() {
+
+  frontJsObj = new frontJs();
+
+
+  var url = window.location;
+  if (url.href.indexOf('weluka') == -1) { //front表示のみ(Welukaビルダーでは動作しない)
+    //frontのみ
+
+
+
+  } else {
+    //ビルダーのみ
+
+  }
+
+});
+/*end.load時にkick*/
+
+/* subJsオブジェクト生成コンストラクタ */
+var frontJs = function() {
+
+  //アンカーを#から？でも使用可能。例）http://url.com/partner/?partner1
+  this.sub_anchor = function() {
+    var query = document.location.search.substring(1);
+    if (query) {
+      var obj = document.getElementById(query);
+      if (obj) {
+        y = obj.offsetTop;
+        scrollTo(0, y);
+      }
+    }
+  }
+
+  /*Sectionの表示効果(http://www.north-geek.com/entry/js-scroll)*/
+
+
+
+}
